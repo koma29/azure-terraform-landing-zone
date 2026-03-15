f6b7222ac12d50e9d7ba100ddf7ec9d199682dee
Cody Marshall <codywademarshall@gmail.com>
2026-03-14 22:54:57 -0700

    Add Terraform variables for location and resource group

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region"
+  default     = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "rg-terraform-landing-zone"
+}
