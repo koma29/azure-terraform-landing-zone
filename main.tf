dbd095400e6050e48a1b718a6c5721fa4cce7e15
Cody Marshall <codywademarshall@gmail.com>
2026-03-20 16:36:11 -0700

    Add Azure virtual network

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "landing_zone" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "landing_vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.landing_zone.name
+  address_space       = var.address_space
+}
