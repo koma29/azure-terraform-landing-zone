64b16783260995a92d7e18dde332a5a4da65bbfb
Cody Marshall <codywademarshall@gmail.com>
2026-03-20 16:31:57 -0700

    Add networking variables for virtual network and subnet

--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "resource_group_name" {
   description = "Name of the resource group"
   default     = "rg-terraform-landing-zone"
 }
+
+variable "vnet_name" {
+  default = "vnet-landing-zone"
+}
+
+variable "subnet_name" {
+  default = "subnet-workload"
+}
+
+variable "address_space" {
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  default = ["10.0.1.0/24"]
+}
