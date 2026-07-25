1964e615be1d8f683b682fca9ae7bbe92eda3e79
Cody Marshall <70298672+koma29@users.noreply.github.com>
2026-07-24 23:32:52 -0700

    Added types to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,31 @@
 variable "location" {
   description = "Azure region"
-  type    = string
+  type        = string
   default     = "eastus"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
+  type        = string
   default     = "rg-terraform-landing-zone"
 }
 
 variable "vnet_name" {
+  type    = string
   default = "vnet-landing-zone"
 }
 
 variable "subnet_name" {
+  type    = string
   default = "subnet-workload"
 }
 
 variable "address_space" {
+  type    = list(string)
   default = ["10.0.0.0/16"]
 }
 
 variable "subnet_prefix" {
+  type    = list(string)
   default = ["10.0.1.0/24"]
 }
